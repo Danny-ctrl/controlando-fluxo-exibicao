e1f9aa8e1e1b4e1ab14ce40bc6765faf4e3dc1d4
Daniele Freitas <71996747+Danny-ctrl@users.noreply.github.com>
2022-05-19 21:18:58 -0300

    criando projeto

---
+++ Aula01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int valor1;
+      int valor2;
+      int total;
+      System.Console.WriteLine("Digite o primeiro valor");
+      valor1 = int.Parse(Console.ReadLine());
+      System.Console.WriteLine("O primeiro valor é: " + valor1);
+      System.Console.WriteLine("Digite o segundo valor");
+      valor2 = int.Parse(Console.ReadLine());
+      System.Console.WriteLine("O segundo valor é: " + valor2);
+
+      total = valor1 + valor2;
+      System.Console.WriteLine("A soma dos valores é: " + total);
+    }
+  }
+}
